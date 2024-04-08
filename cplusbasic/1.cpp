37fbd4c79e41391f5b34ebc3319cc6766935cab4
kwonjuyeong <20161527@vision.hoseo.edu>
2024-04-08 22:42:11 +0900

    1장 입출력

---
+++ cplusbasic/1.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+
+int main(void)
+{
+    /*
+    1. 출력문 예제
+    int num = 20;
+    std::cout << "Hello World!"<<std::endl;
+    std::cout << "Hello "<<"World!"<< std::endl;
+    std::cout << num<< ' '<<'A';
+    std::cout << " " <<3.14<<std::endl;
+
+    2. 입력문 예제
+    int val1;
+    std::cout << "첫 번째 숫자입력: ";
+    std::cin >> val1;
+
+    int val2;
+    std::cout << "두 번째 숫자입력: ";
+    std::cin >> val2;
+
+    int result = val1 + val2;
+    std::cout << "덧셈결과: " << result << std::endl;
+
+    3. 배열 기반의 문자열 입출력
+    char name[100];
+    char lang[200];
+
+    std::cout << "이름은 무엇입니까?";
+    std::cin >> name;
+
+    std::cout << "좋아하는 프로그래밍 언어는 무엇인가요?";
+    std::cin >> lang;
+
+    std::cout << "내 이름은 " << name << "입니다.\n";
+    std::cout << "제일 좋아하는 언어는 " << lang << "입니다.\n";
+ */
+
+
+ /* 01-1
+  문제1
+  int num[5];
+  int total;
+
+  for(int i = 0; i < 5; i++)
+  {
+     std::cout<<i+1<<"번째 정수 입력:";
+     std::cin>>num[i];
+     total += num[i];
+  }
+  std::cout<<"합계:"<<total<<std::endl;
+
+  문제2
+  char name[20], phone[30];
+  std::cin>>name>>phone;
+  std::cout<<name<<" "<<phone;
+
+  문제3
+  int gugu;
+  std::cout<<"구구단을 외자!\n숫자를 입력해주세요 : ";
+  std::cin>>gugu;
+
+  for(int i = 1; i < 10; i++){
+  std::cout<<gugu<<"x"<<i<<" = "<<gugu*i<<std::endl;
+  }
+
+  문제4
+  int num;
+  int salary;
+  while (num!= -1){
+  std::cout<<"판매 금액을 만원 단위로 입력(-1 to end): ";
+  std::cin>>num;
+  salary = 50 + (num*0.12);
+  std::cout<<"이번 달 급여 : "<<salary<<"만원\n";
+  }
+  */
+
+}
